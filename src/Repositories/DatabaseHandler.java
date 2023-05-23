4cc4ab52dd5d7908dfd2a74628ca9fccf082121e
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-05-24 00:11:42 +0300

    Update DatabaseHandler.java

--- src/Repositories/DatabaseHandler.java
+++ src/Repositories/DatabaseHandler.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DatabaseHandler {
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/pao_library_db";
     private static final String DATABASE_USER = "root";
-    private static final String DATABASE_PASSWORD = "Alex@908";
+    private static final String DATABASE_PASSWORD = " ";
 
 
     public static Connection getConnection() throws SQLException {
